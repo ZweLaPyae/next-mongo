6881c2e6d192acea29d01830b648ec41b7ac5fa5
ZweLaPyae <u6530207@au.edu>
2024-09-14 19:25:03 +0700

    class13 updates 2

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,7 +1,7 @@
 import Category from "@/models/Category";
 
 export async function GET() {
-  const categories = await Category.find().sort({ name: 1 })
+  const categories = await Category.find().sort({ order: -1 })
   return Response.json(categories)
 }
 
@@ -17,6 +17,6 @@ export async function POST(request) {
 // for V2
 export async function PUT(request) {
   const body = await request.json()
-  const category = await Category.findByIdAndUpdate(body._id, body) 
+  const category = await Category.findByIdAndUpdate(body._id, body)
   return Response.json(category)
 }
--- app/category/page.js
+++ app/category/page.js
@@ -2,63 +2,152 @@
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
-
+import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
+ 
 export default function Home() {
   const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+  console.log(process.env.NEXT_PUBLIC_API_URL)
   const [categoryList, setCategoryList] = useState([]);
-  const { register, handleSubmit } = useForm();
+  const [editMode, setEditMode] = useState(false);
+  const { register, handleSubmit, reset } = useForm();
 
+  const columns = [
+    { field: 'name', headerName: 'Category', width: 200 },
+    { field: 'order', headerName: 'Order', width: 100 },
+    {
+      field: "action", headerName: "Action", width: 100,
+      renderCell: (params) =>
+        <Button
+          onClick={() => startEditMode(params.row)}
+          className="border border-gray-700 px-2 m-1">📝</Button>
+    }
+  ];
+ 
   async function fetchCategory() {
     const data = await fetch(`${API_BASE}/category`);
     const c = await data.json();
-    setCategoryList(c);
+    const c2 = c.map((category) => {
+      category.id = category._id;
+      return category;
+    })
+    setCategoryList(c2);
   }
-
+ 
   useEffect(() => {
     fetchCategory();
   }, []);
-
-  function createCategory(data) {
+ 
+  function createOrUpdateCategory(data) {
+    if (editMode) {
+      // Update category
+      fetch(`${API_BASE}/category`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then(() => {
+        stopEditMode()
+        fetchCategory()
+      });
+      return
+    }
+ 
     fetch(`${API_BASE}/category`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then(() => fetchCategory());
+    }).then(() => {
+      stopEditMode()
+      fetchCategory()
+    });
   }
-
+ 
+  function startEditMode(category) {
+    reset(category)
+    setEditMode(true);
+  }
+ 
+  function stopEditMode() {
+    reset({
+      name: '',
+      order: ''
+    })
+    setEditMode(false)
+  }
+ 
   return (
     <main>
-      <form onSubmit={handleSubmit(createCategory)}>
-        <div className="grid grid-cols-2 gap-4 w-fit m-4">
-          <div>Category:</div>
+      <form onSubmit={handleSubmit(createOrUpdateCategory)}>
+        <div className="grid grid-cols-2 gap-4 w-fit m-4 border border-red-600 p-4">
+          <div>Category Name:</div>
           <div>
             <input
               name="name"
               type="text"
               {...register("name", { required: true })}
-              className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              className="border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
           </div>
-          <div className="col-span-2">
+          <div>Order:</div>
+          <div>
             <input
-              type="submit"
-              value="Add"
-              className="italic bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              name="order"
+              type="number"
+              {...register("order", { required: true })}
+              className="border border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
+          </div>
+          <div className="col-span-2 text-right">
+            {editMode ?
+              <>
+                <input
+                  type="submit"
+                  value="Update"
+                  className="italic bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                />
+                {' '}
+                <button
+                onClick={()=>stopEditMode()}
+                className="italic bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+                >Cancel</button>
+              </>
+              :
+              <input
+                type="submit"
+                value="Add"
+                className="italic bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+              />
+            }
+ 
+ 
           </div>
         </div>
       </form>
       <div>
         <h1>Category ({categoryList.length})</h1>
-        {categoryList.map((category) => (
-          <div key={category._id}>
-            <Link href={`/product/category/${category._id}`} className="text-red-600">
-              {category.name}
-            </Link>
-          </div>
-        ))}
+
+        <div className="mx-4 border border-gray-600">
+          <DataGrid
+            columns={columns}
+            rows={categoryList} />
+        </div>
+        
+        <ul>
+          {categoryList.map((category) => (
+            <li key={category._id}>
+              <button
+                onClick={() => startEditMode(category)}
+                className="border border-gray-700 px-2 m-1">📝</button>
+              <Link href={`/product/category/${category._id}`} className="text-red-600">
+                {category.name} [{category.order}]
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
